4df55901878a935211da2fec75a0c3ad7e1f2680
Juliette Decugis <jdecugis@berkeley.edu>
2024-03-19 13:25:43 +0100

    models
    with models

---
+++ loss_functions.py
@@ -0,0 +1,110 @@
+import torch
+import torch.nn as nn
+from torch.autograd.function import Function
+import torch
+
+# Code taken from:
+# source: https://github.com/jxgu1016/MNIST_center_loss_pytorch/blob/master/CenterLoss.py
+
+class CenterLoss(nn.Module):
+    def __init__(self, num_classes, feat_dim, size_average=True):
+        super(CenterLoss, self).__init__()
+        self.centers = nn.Parameter(torch.randn(num_classes, feat_dim))
+        self.centerlossfunc = CenterlossFunc.apply
+        self.feat_dim = feat_dim
+        self.size_average = size_average
+
+    def forward(self, label, feat):
+        batch_size = feat.size(0)
+        feat = feat.view(batch_size, -1)
+        # To check the dim of centers and features
+        if feat.size(1) != self.feat_dim:
+            raise ValueError("Center's dim: {0} should be equal to input feature's \
+                            dim: {1}".format(self.feat_dim,feat.size(1)))
+        batch_size_tensor = feat.new_empty(1).fill_(batch_size if self.size_average else 1)
+        loss = self.centerlossfunc(feat, label, self.centers, batch_size_tensor)
+        return loss
+
+
+class CenterlossFunc(Function):
+    @staticmethod
+    def forward(ctx, feature, label, centers, batch_size):
+        ctx.save_for_backward(feature, label, centers, batch_size)
+        centers_batch = centers.index_select(0, label.long())
+        return (feature - centers_batch).pow(2).sum() / 2.0 / batch_size
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        feature, label, centers, batch_size = ctx.saved_tensors
+        centers_batch = centers.index_select(0, label.long())
+        diff = centers_batch - feature
+        # init every iteration
+        counts = centers.new_ones(centers.size(0))
+        ones = centers.new_ones(label.size(0))
+        grad_centers = centers.new_zeros(centers.size())
+
+        counts = counts.scatter_add_(0, label.long(), ones)
+        grad_centers.scatter_add_(0, label.unsqueeze(1).expand(feature.size()).long(), diff)
+        grad_centers = grad_centers/counts.view(-1, 1)
+        return - grad_output * diff / batch_size, None, grad_centers / batch_size, None
+    
+class FocalLoss(nn.Module):
+    def __init__(self, alpha, gamma):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+    
+    def forward(self, label, feats):
+        return sigmoid_focal_loss(label, feats, self.alpha, self.gamma)
+
+## Code adapted from: https://github.com/facebookresearch/fvcore/blob/main/fvcore/nn/focal_loss.py
+    
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+# pyre-strict
+        
+def sigmoid_focal_loss(
+    inputs: torch.Tensor,
+    targets: torch.Tensor,
+    alpha: float = -1,
+    gamma: float = 2,
+    reduction: str = "mean",
+) -> torch.Tensor:
+    """
+    Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
+    Args:
+        inputs: A float tensor of arbitrary shape.
+                The predictions for each example.
+        targets: A float tensor with the same shape as inputs. Stores the binary
+                 classification label for each element in inputs
+                (0 for the negative class and 1 for the positive class).
+        alpha: (optional) Weighting factor in range (0,1) to balance
+                positive vs negative examples. Default = -1 (no weighting).
+        gamma: Exponent of the modulating factor (1 - p_t) to
+               balance easy vs hard examples.
+        reduction: 'none' | 'mean' | 'sum'
+                 'none': No reduction will be applied to the output.
+                 'mean': The output will be averaged.
+                 'sum': The output will be summed.
+    Returns:
+        Loss tensor with the reduction option applied.
+    """
+    inputs = inputs.float()
+    # targets = targets.float()
+    p = torch.sigmoid(inputs)[:, 0]
+    ce_loss_func = nn.CrossEntropyLoss(reduction='none')
+    ce_loss = ce_loss_func(inputs, targets.long())
+
+    p_t = p * targets + (1 - p) * (1 - targets)
+    loss = ce_loss * ((1 - p_t) ** gamma)
+
+    if alpha >= 0:
+        alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
+        loss = alpha_t * loss
+    
+    if reduction == "mean":
+        loss = loss.mean()
+    elif reduction == "sum":
+        loss = loss.sum()
+
+    return loss
---
+++ main.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn as nn
+from Fusion_Model import Transformer, FusionModel
+from torch.utils.data import DataLoader, TensorDataset
+from transformers import BertModel, BertConfig
+from CenterLoss import FocalLoss, CenterLoss
+from tqdm import tqdm
+from sklearn.metrics import f1_score
+
+class Fine_Tuned_Bert(nn.Module):
+    def __init__(self, input_size, num_classes):
+        num_classes = 64
+        super(Fine_Tuned_Bert, self).__init__()
+        self.fc1 = nn.Linear(input_size, 768)
+        self.fc = nn.Linear(768, num_classes)
+        bert_config = BertConfig.from_pretrained('bert-base-uncased')
+        self.model = BertModel.from_pretrained('bert-base-uncased', config=bert_config).encoder
+        self.softmax = nn.Softmax(dim=1)
+        self.dropout = nn.Dropout()
+
+    def forward(self, x):
+        x = x.float()
+        x = self.fc1(x)
+        x = self.model(x.unsqueeze(0)).last_hidden_state
+        x = self.fc(x)
+        # x = self.softmax(x)
+        return x
+    
+def get_dataloaders(X, y, batch_size=None):
+    dataset = TensorDataset(X, y)
+    if batch_size is None:
+        dataloader = DataLoader(dataset, batch_size=X.shape[0], shuffle=True)
+    else:
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return dataloader
+
+def train(NUM_EPOCHS, dataloader, model, test_data):
+    criterion = CenterLoss(4, 64)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    # Training loop
+    model.train()
+    for epoch in tqdm(range(NUM_EPOCHS)):
+        for inputs, labels in dataloader:
+            # Forward pass
+            predictions = model(inputs)[0]
+            # Compute loss
+            loss = criterion(labels, predictions)
+
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        if epoch % 2 == 0:
+            print('Epoch loss:', loss.item())
+            # print('F1 train score: ', f1_score(torch.argmax(predictions, dim = 1), labels, average = 'macro'))
+            X, y = next(iter(test_data))
+            preds = model(X)[0]
+            predTest = model(X)[0]
+            print('Val loss ', criterion(y, X))
+            # print('F1 test score: ', f1_score(torch.argmax(predTest, dim = 1), y, average = 'macro'))
+            torch.save(model.state_dict(), 'bert_fine_tuned' + str(epoch) + '.pt')
+
+def main(X, y, Xtest, ytest, embeddings=True):
+    # if embeddings:
+    #     fusion_model = FusionModel(input_size=32, num_layers=3, projected_size=32, hidden_size=64)
+    #     fusion_model.load_state_dict(torch.load('fusion_model_128_3.pt'))
+    #     fusion_model.eval()
+    #     X = fusion_model(X)
+    # focal_loss = FocalLoss(alpha = 0.9, gamma = 0.1)
+    data = get_dataloaders(X, y, batch_size=128)
+    test_data = get_dataloaders(Xtest, ytest, batch_size=128)
+    model = Fine_Tuned_Bert(X.shape[1], 4)
+    # Freeze the first 6 layers
+    for param in model.parameters():
+        param.requires_grad = False
+    # Make sure the last few layers are trainable
+    for i in range(4):
+        for param in model.model.layer[-i].parameters():
+            param.requires_grad = True
+    train(10, data, model, test_data)
+    torch.save(model.state_dict(), 'bert_fine_tuned.pt')
+
+if __name__ == "__main__":
+    X_train, y_train = torch.load('data/X_train_simple.pt'), torch.load('data/y_train.pt')
+    cutoff = int(X_train.shape[0]*0.7)
+    X_train1, y_train1 = X_train[:cutoff, :], y_train[:cutoff]
+    X_val, y_val = X_train[cutoff:, :], y_train[cutoff:]
+    X_train, y_train = X_train1, y_train1
+    X_train.shape, X_val.shape
+    # X_test, y_test = torch.load('data/X_test_simple.pt'), torch.load('data/y_test.pt')
+    main(X_train, y_train, X_val, y_val)
